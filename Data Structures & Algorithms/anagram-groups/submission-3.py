939cfd3664bc9204ef4afeb443dbfe0becd6fb35
Eric Sadowski <esadowsk@purdue.edu>
2026-09-18 01:30:18 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {} # map freq list to list of strings
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) not in res:
+                res[tuple(count)] = []
+            res[tuple(count)].append(s)
+        return list(res.values())
